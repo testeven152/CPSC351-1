31c85b3d503ee4c7197964e82063ef04c348827c
Steven Te <testeven152@gmail.com>
2019-04-18 19:15:27 -0700

    main

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+class Address 
+{
+    //store
+    //retrieve
+    
+    void getPage();
+    void getOffset();
+};
+
+class replacementalgo 
+{
+
+};
+
+class fifo : public replacementalgo
+{
+
+};
+
+class lru : public replacementalgo
+{
+
+};
+
+
+
+int main() {
+/* 
+    while(address) {
+        translate();
+        readfromRAM();
+        writestdoutput();
+    }
+ */
+
+}
