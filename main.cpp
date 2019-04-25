d1da7e30ca6be779b9925b690b9838669e70a14c
Steven Te <testeven152@gmail.com>
2019-04-24 19:49:36 -0700

    added headers to main

--- main.cpp
+++ main.cpp
@@ -1,28 +1,13 @@
 #include <iostream>
 
-class Address 
-{
-    //store
-    //retrieve
-    
-    void getPage();
-    void getOffset();
-};
-
-class replacementalgo 
-{
-
-};
-
-class fifo : public replacementalgo
-{
-
-};
-
-class lru : public replacementalgo
-{
-
-};
+// May not need all headers to be included
+#include "Hardware/MMU.hpp"
+#include "Hardware/RAM.hpp"
+#include "Hardware/Word.hpp"
+#include "OperatingSystem/MemoryManager.hpp"
+#include "OperatingSystem/PCB.hpp"
+#include "OperatingSystem/PageReplacementAlgorithm.hpp"
+#include "OperatingSystem/PageTableEntry.hpp"
 
 
 
