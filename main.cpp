8dbdaeeb6c166354f22ba7dde5a05fa21f1205a0
Steven Te <testeven152@gmail.com>
2019-05-09 12:46:13 -0700

    main changes

--- main.cpp
+++ main.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
 
+
+
 // May not need all headers to be included
 #include "Hardware/MMU.hpp"
-#include "Hardware/RAM.hpp"
-#include "Hardware/Word.hpp"
 #include "OperatingSystem/MemoryManager.hpp"
 #include "OperatingSystem/PCB.hpp"
-#include "OperatingSystem/PageReplacementAlgorithm.hpp"
-#include "OperatingSystem/PageTableEntry.hpp"
+
+const int PAGE_SIZE = 256;
+const int FRAME_SIZE = 256;
+const int FRAMES = 256;
+const int TLB_SIZE = 16;
 
 
+unsigned int logicaladdress;
+unsigned char data;
+
+ProcessControlBlock PCB;
+MemoryManagementUnit MMU;
+MemoryManager MM;
 
 int main() {
+
+
+    while(std::cin >> logicaladdress) {
+        try {
+            MMU.read(logicaladdress, PCB, data);
+            std::cout << data;
+            break;
+        }
+        catch(PageFault pf) {
+            MM.pageIn(logicaladdress, PCB.myPageTable);
+        }
+    };
+
 
 };
