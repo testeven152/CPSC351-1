f13361f7e6242e4b49b94518fd674a917547ae50
Steven Te <testeven152@gmail.com>
2019-05-12 15:40:32 -0700

    implemented freeframeslist

--- OperatingSystem/MemoryManager.cpp
+++ OperatingSystem/MemoryManager.cpp
@@ -1,10 +1,14 @@
 #include "MemoryManager.hpp"
 
+MemoryManager::MemoryManager() {
+    freeFrames_(256);
+}
+
 void MemoryManager::pageIn(struct Word pagenumber, struct ProcessControlBlock& pcb){
     if(!freeFrames_.empty()) {
         // need to implement freeframeslist before implementing this function
         // framenumber = freeFrames_.front();
-        // freeFrames_.pop.front();
+        // freeFrames_.pop();
     } else {
         // pcb.PageTable[pagenumber.value_] = framenumber;
         // framenumber = true;
--- OperatingSystem/MemoryManager.hpp
+++ OperatingSystem/MemoryManager.hpp
@@ -6,17 +6,47 @@
 class MemoryManager {
     public:
         // MemoryManager& instance();
-        // MemoryManager();
+        MemoryManager();
         // MemoryManager& operator=();
         void pageIn(struct Word pagenumber, struct ProcessControlBlock& pcb);
     private:
-        struct FreeFrameList freeFrames_;
+        struct FreeFrameList freeFrames_(256);
         // MemoryManager();
 
 };
 
-struct FreeFrameList {
-        bool empty();
-        // front();
+struct FreeFrameList { //implemented as a queue
+
+    int front, rear, size;
+    int* queue;
+    FreeFrameList(int s)
+    {
+        queue = new int;
+        size = s;
+        for(int i = 0; i < s; i++) {
+            queue[i] = i + 1;
+            rear++;
+        }
+    }
+
+    ~FreeFrameList() {
+        delete[] queue;
+    }
+
+    void pop(){
+        for (int i = 0; i < rear - 1; i++) {
+            queue[i] = queue[i+1];
+        }
+        rear--;
+    }
+
+    int front(){
+        return front;
+    }
+
+    bool empty(){
+        if (front == rear) return true;
+        else return false;
+    }
 
 };
