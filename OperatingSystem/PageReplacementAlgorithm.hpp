21bf1f9aebe2ed9fb699360885092aeddc13c5ef
IWearAThai <tkluu@csu.fullerton.edu>
2019-05-12 05:29:12 -0700

    PRA
    Think i did the PRA correctly.

--- OperatingSystem/PageReplacementAlgorithm.hpp
+++ OperatingSystem/PageReplacementAlgorithm.hpp
@@ -1,11 +1,14 @@
 #pragma once
+#include <"TLB.h">
+
 
 class PageReplacementAlgorithm {
     public:
         // ~PageReplacementAlgorithm();
         // Word findVictim();
         // PageReplacementAlgorithm& getPRA();
-        // void updateUsage();
+        void updateUsageLRU(TLBentry);
+		void updateUsageFIFO(TLBentry);
     protected:
         // PageReplacementAlgorithm();
     private:
@@ -23,8 +26,34 @@ class FirstInFirstOutAlgorithm : public PageReplacementAlgorithm {
 class LeastRecentlyUsedAlgorithm : public PageReplacementAlgorithm {
     public:
         // Word findVictim();
-        // LeastRecentlyUsedAlgorithm();
+        //LeastRecentlyUsedAlgorithm();
         // void updateUsage();
     private:
         // UsedList lruList;
 };
+
+
+/*
+This is the PageReplacementAlgorithm.cpp file implementation below
+*/
+
+void PageReplacementAlgorithm::updateUsageLRU(TLBentry Ltable) {
+	for (int i = table.pagenumber; i < 16; i++)
+	{
+		table[i].pagenumber = table[i + 1].pagenumber;
+		table[i].framenumber = table[i + 1].framenumber;
+	}
+	
+
+	table[16] = Ltable;
+	// Still have to "cout" something here or not
+}
+
+void PageReplacementAlgorithm::updateUsageFIFO(TLBentry FTable) {
+	for (int i = 0; i < 15; i++)
+	{
+		table[i] = table[i + 1];
+	}
+	table[16] = FTable;
+	// Still have to "cout" something here or not
+}
